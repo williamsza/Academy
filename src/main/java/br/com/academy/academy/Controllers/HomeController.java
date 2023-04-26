82491aee528c3612175cd5edfb5090874b622d1e
William de Souza <william.sza@outlook.com>
2023-04-26 11:12:16 -0300

    Atualizações

---
+++ src/main/java/br/com/academy/academy/Controllers/HomeController.java
@@ -0,0 +1,20 @@
+package br.com.academy.academy.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+
+@GetMapping("/")
+    public ModelAndView index(){
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
+        mv.addObject("msg","Mensagem recebida diretamente do controller");
+        return mv;
+
+    }
+    
+}
